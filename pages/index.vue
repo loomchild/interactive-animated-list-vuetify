e0b4d5ab2fa83ff34a4ecadd1ccc65b0507e9ba6
Jarek Lipski <loomchild@loomchild.net>
2020-03-08 18:39:11 +0100

    remove unnecessary text field type

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,12 @@
             <card-list v-model="items" #default="{ item }">
               <v-row>
                 <v-col cols="12">
-                  <v-text-field v-model="item.name" label="Name" type="string" hide-details />
+                  <v-text-field v-model="item.name" label="Name" hide-details />
                 </v-col>
               </v-row>
               <v-row>
                 <v-col cols="12">
-                  <v-text-field v-model="item.email" label="Email" type="string" hide-details />
+                  <v-text-field v-model="item.email" label="Email" hide-details />
                 </v-col>
               </v-row>
             </card-list>
