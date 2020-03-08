2b66d148cc2fe976d6a94304151873198059829c
Jarek Lipski <loomchild@loomchild.net>
2020-03-08 18:49:43 +0100

    improve client code

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,29 @@
 <template>
-  <v-container full-height>
+  <v-container full-height class="py-0">
     <v-row>
-      <v-col cols="6" offset="3">
-        <v-row>
-          <v-col cols="12">
-            <card-list v-model="items" #default="{ item }">
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field v-model="item.name" label="Name" hide-details />
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field v-model="item.email" label="Email" hide-details />
-                </v-col>
-              </v-row>
-            </card-list>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="auto">
-            <v-btn @click="add" text>
-              <v-icon>
-                mdi-plus
-              </v-icon>
-              Add
-            </v-btn>
-          </v-col>
-        </v-row>
+      <v-col cols="12">
+        <card-list v-model="items" #default="{ item }">
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="item.name" label="Name" hide-details />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="item.email" label="Email" hide-details />
+            </v-col>
+          </v-row>
+        </card-list>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="auto" class="py-0 mx-auto">
+        <v-btn @click="add" text>
+          <v-icon>
+            mdi-plus
+          </v-icon>
+          Add
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
